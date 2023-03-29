dcb2de2d003c31244a927fb432f50742456baae7
Juugo11 <leoemmanuel91@gmail.com>
2023-03-29 07:03:43 -0700

    Boss

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ *
+ * Return: char value
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int dlen = 0, i;
+	while (dest[dlen])
+	{
+	    dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
+
+
+	
+
