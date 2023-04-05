923e6680794c3ab7a0e65bba294083f24cdf4cc1
Juugo11 <leoemmanuel91@gmail.com>
2023-04-05 08:38:54 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,6 +6,14 @@
  *
  * Return: natural square root, -1 if n does not have a natural squareroot
  */
+
+/**
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
+ * @root: test this root
+ * Return: natural square root, or -1 if not natural root
+ */
+
 int _find_root(int n, int root)
 {
 	if (root * root > n)
@@ -14,7 +22,7 @@ int _find_root(int n, int root)
 	if (root * root == n)
 		return (root);
 
-	return(_find_root(n,root + 1));
+	return(_find_root(n, root + 1));
 }
 
 
@@ -23,5 +31,5 @@ int _sqrt_recursion(int n)
 	if(n < 0)
 		return (-1);
 
-	return (_find_root(n,0));
+	return (_find_root(n, 0));
 }
